039047c74469e6ff4092b46a8d0ce576bdd12d3a
sung-chu <songzhu1030@gmail.com>
2024-08-30 19:54:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,17 +110,20 @@ with col4:
         st.info("30 days survival rate is {:.2f}%".format((1 - SVC_result[0])*100))
         
 st.markdown('''---''')
-st.write('**About the creator**')
-st.write("Chien-Chang Lee MD, ScD Chien-Chang Lee MD, ScD, is the Director of Department of Information Management, Ministry of Health and Welfare. He is also a physician in Department of Emergency Medicine at National Taiwan University Hospital and vice deputy of Health Data Science Research Group at National Taiwan University Hospital.")
-st.write('**When to use**')
-st.write("The FluDeep score is the first stratification schema combining chest X-ray and clinical data helps determine the 30 day risk of mortality in specifically targeting patients with influenza pneumonia. The FluDeep score can be used to provide an idea of a patient’s distributions in the emergency department and primary care clinics.")
-st.write("**Interpretation of FluDeep-XR score**")
+st.write("""**About the creator**  
+Chien-Chang Lee MD, ScD Chien-Chang Lee MD, ScD, is the Director of Department of Information Management, Ministry of Health and Welfare. He is also a physician in Department of Emergency Medicine at National Taiwan University Hospital and vice deputy of Health Data Science Research Group at National Taiwan University Hospital.
+""")
+st.write("""**When to use**  
+The FluDeep score is the first stratification schema combining chest X-ray and clinical data helps determine the 30 day risk of mortality in specifically targeting patients with influenza pneumonia. The FluDeep score can be used to provide an idea of a patient’s distributions in the emergency department and primary care clinics.
+""")
 st.write("""
+**Interpretation of FluDeep-XR score**  
 1 for Normal lung  
 2 for Hyperinflation and/or bronchial wall thickening  
 3 for Focal alveolar consolidation limited in a single lobe  
 4 for Multifocal consolidation  
 5 for Diffuse alveolar consolidation and/or pleural effusion involving the upper lobe on at least one lung  
 """)
-st.write("**Evidence appraisal**")
-st.write("The FluDeep was derived from a retrospective observational cohort study based upon antigen positively tested influenza patients admitted to  tertiary level hospitals in Taiwan and Italy. The score includes a chest X-ray and ninteen variables. The system predicts 30 day in-hospital survival rate. The score performed well in the internal validation of the study along with external validation studies. Overall generalizability of the score needs to be further investigated as it was derived and validated in a primarily homogeneous ethnic and age cohort.")
+st.write("""**Evidence appraisal**
+The FluDeep was derived from a retrospective observational cohort study based upon antigen positively tested influenza patients admitted to  tertiary level hospitals in Taiwan and Italy. The score includes a chest X-ray and ninteen variables. The system predicts 30 day in-hospital survival rate. The score performed well in the internal validation of the study along with external validation studies. Overall generalizability of the score needs to be further investigated as it was derived and validated in a primarily homogeneous ethnic and age cohort.
+""")
