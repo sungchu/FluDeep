a160284fc0ec67b2a54f6e530853b0ce1fb08eb3
sungchu <songzhu1030@gmail.com>
2025-08-18 16:56:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,6 +91,7 @@ with col4:
         import gdown
         url = "https://drive.google.com/file/d/1b_3F8HWlvQMBFden0am-FC372dfd16VS/view?usp=sharing"
         output = "xception_mse0.8931_val_mse1.2649_new.hdf5"
+        st.write(os.access(output, os.R_OK))
         gdown.download(url, output, quiet=False)
         model = tf.keras.models.load_model(output)
 
